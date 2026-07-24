70c56fc52549f64a05be39229e53c5763426dcc2
madhav-naradala <39063888+madhav-naradala@users.noreply.github.com>
2026-07-23 21:20:20 -0500

    Update index.ts

--- components/Resume/index.ts
+++ components/Resume/index.ts
@@ -1,6 +1,24 @@
-export { ResumeHeader } from './ResumeHeader';
-export { ExperienceSection } from './ExperienceSection';
-export { SkillsSection } from './SkillsSection';
-export { EducationSection } from './EducationSection';
-export { CertificationsSection } from './CertificationsSection';
-export { Badge } from './Badge';
+export {
+    ResumeHeader
+}
+from './ResumeHeader';
+export {
+    ExperienceSection
+}
+from './ExperienceSection';
+export {
+    SkillsSection
+}
+from './SkillsSection';
+export {
+    EducationSection
+}
+from './EducationSection';
+export {
+    CertificationsSection
+}
+from './CertificationsSection';
+export {
+    Badge
+}
+from './Badge';
