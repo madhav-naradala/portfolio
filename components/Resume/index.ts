245a5e38d242e948fb6b18d7b5784a1d8385c290
madhav-naradala <39063888+madhav-naradala@users.noreply.github.com>
2026-07-23 22:18:40 -0500

    Clean resume component exports

--- components/Resume/index.ts
+++ components/Resume/index.ts
@@ -1,24 +1,5 @@
-export {
-    ResumeHeader
-}
-from './ResumeHeader';
-export {
-    ExperienceSection
-}
-from './ExperienceSection';
-export {
-    SkillsSection
-}
-from './SkillsSection';
-export {
-    EducationSection
-}
-from './EducationSection';
-export {
-    CertificationsSection
-}
-from './CertificationsSection';
-export {
-    Badge
-}
-from './Badge';
+export { ResumeHeader } from './ResumeHeader';
+export { ExperienceSection } from './ExperienceSection';
+export { SkillsSection } from './SkillsSection';
+export { EducationSection } from './EducationSection';
+export { Badge } from './Badge';
